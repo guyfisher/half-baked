a30142d356c6a7e71b05ae2f367d30a058186f1a
guy@guyfisher.com <guy@guyfisher.com@f9d38d7f-ec01-2504-6416-8c4cede3ed4c>
2013-01-07 21:29:10 +0000

    Branched version 1.6.3

---
+++ branches/1.6.3/meta.php
@@ -0,0 +1,5 @@
+			<p>
+				<?php bloginfo('name') ?> is&nbsp;powered by&nbsp;<a href="http://wordpress.org/">WordPress</a><br />
+				<a href="http://wordpress.org/extend/themes/half-baked" title="Half-Baked WordPress Theme">Half-Baked</a>&nbsp;design by&nbsp;Guy
+			</p>
+			<p class="login">[ <?php wp_register('', ' | '); wp_loginout(); ?> ]</p>
