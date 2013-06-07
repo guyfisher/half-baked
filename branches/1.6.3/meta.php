c9e524ba835e8c14838c31ad2f5205bd91895172
guy@guyfisher.com <guy@guyfisher.com@f9d38d7f-ec01-2504-6416-8c4cede3ed4c>
2013-06-07 20:35:37 +0000

    Updated accordion and meta widgets styles

--- branches/1.6.3/meta.php
+++ branches/1.6.3/meta.php
@@ -1,3 +1,3 @@
-			<p><?php bloginfo( 'name' ); ?> is&nbsp;powered by&nbsp;<a href="http://wordpress.org/">WordPress</a>&nbsp;and&nbsp;<a href="http://wordpress.org/extend/themes/half-baked" title="Half-Baked WordPress Theme">Half-Baked</a></p>
+			<p><?php bloginfo( 'name' ); ?> is&nbsp;powered by&nbsp;<a href="http://wordpress.org/">WordPress</a> and&nbsp;<a href="http://wordpress.org/extend/themes/half-baked" title="Half-Baked WordPress Theme">Half-Baked</a></p>
 			<p class="login">[ <?php wp_register( '', ' | ' ); wp_loginout(); ?> ]</p>
 			<ul><?php wp_meta(); /* meta api hook */ ?></ul>
