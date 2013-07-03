bd8ab739c7ad56665ee93ff76d16ffb8633ead7b
guy@guyfisher.com <guy@guyfisher.com@f9d38d7f-ec01-2504-6416-8c4cede3ed4c>
2013-07-03 17:40:43 +0000

    Delete version 1.6.3 branch

--- branches/1.6.3/meta.php
+++
@@ -1,3 +0,0 @@
-			<p><?php bloginfo( 'name' ); ?> is&nbsp;powered by&nbsp;<a href="http://wordpress.org/">WordPress</a> and&nbsp;<a href="http://wordpress.org/extend/themes/half-baked" title="Half-Baked WordPress Theme">Half-Baked</a></p>
-			<p class="login">[ <?php wp_register( '', ' | ' ); wp_loginout(); ?> ]</p>
-			<ul><?php wp_meta(); /* meta api hook */ ?></ul>
