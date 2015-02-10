3cfb6aa89a79ae3ebedcb38899544fa3bc509da5
Guy Fisher <guyfisher@yahoo.com>
2015-02-10 19:10:28 +0000

    Fixed TypeError in Scriptaculous DOM loaded function

--- scripts/half-baked.js
+++ scripts/half-baked.js
@@ -7,5 +7,7 @@ document.observe("dom:loaded", function() {
 		$("postmeta_slider").hide();
 		$("postmeta_toggle").onclick = function() { new Effect.toggle($("postmeta_slider"), "slide", { duration: 0.5 }); return false; }
 	}
-	$$("div.widget_half_baked_accordion").first().visualEffect("accordian", { headingSelector: "h3", sectionSelector: "div.widget_content" });
+	if ($$("div.widget_half_baked_accordion").first()) {
+		$$("div.widget_half_baked_accordion").first().visualEffect("accordian", { headingSelector: "h3", sectionSelector: "div.widget_content" });
+	}
 });
