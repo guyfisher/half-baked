2ee21279f0fc56debddcac201c16a1f807474767
Guy Fisher <guyfisher@yahoo.com>
2015-10-28 15:15:00 -0400

    Added translation functions to header and footer
    Added translation functions to header and footer templates

--- footer.php
+++ footer.php
@@ -9,8 +9,6 @@
 	<?php wp_footer(); echo("\n"); /* Plugin API Hook */ ?>
 </div><!-- End of Running Foot -->
 
-<?php echo("<!-- $wpdb->num_queries Queries ("); timer_stop(1); echo(" sec.) -->\n"); ?>
-
 </body>
 
 </html>
